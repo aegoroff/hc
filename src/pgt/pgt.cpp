ba0e38c43217ee4d8650e9d9a7799cac4b83e46d
egr <devnull@localhost>
2009-05-16 14:58:54 +0000

    Unit tests project created

---
+++ src/pgt/pgt.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+
+#include "gtest.h"
+
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	// ѕринудительно печатаем врем€ работы тестов.
+	testing::GTEST_FLAG(print_time) = true;
+	return RUN_ALL_TESTS();
+}
