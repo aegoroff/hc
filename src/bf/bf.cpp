58a063d41bd8f7b9eae985ff25d112b6dc8ee2b9
egr <devnull@localhost>
2010-04-08 21:23:12 +0400

    right concept of brute force

--- src/bf/bf.cpp
+++ src/bf/bf.cpp
@@ -3,104 +3,104 @@
 
 #include "stdafx.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
 
-char *bruteforce(int passmax, int passmin);
+static char *alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-int count = 0;
-char *passwd;
-
-#define MAXCHAR 50
-#define MINCHAR 48
-
-/* this code is copyrighted by Hi_tech_assassin, see pscode.com */
-
-char *bruteforce(int passmax, int passmin)
+int MakeAttempt(int pos, int length, const char *pDict, int *indexes, char *pass, const char *desired,
+                unsigned long long *attempts, int maxIndex)
 {
-	char *pass = (char *)malloc(passmin + 1);
-	int position, x, found;
+    int i = 0;
+    int j = 0;
 
-	/* since we can only do one increment per 
-	   iteration we need a way of controling this */
+    for (; i <= maxIndex; ++i) {
+        indexes[pos] = i;
 
-	memset(pass, MINCHAR, passmin);
-	pass[passmin] = '\0';
-
-	for (x = passmin; x <= passmax; ++x) {
-		if (x > passmin) {
-			realloc(pass, x + 1);
-			memset(pass, MINCHAR, x);
-			pass[x] = '\0';
-		}
-
-		while (pass[0] <= MAXCHAR) {
-			found = 0;
-			printf("%s\n", pass);
-            if (strcmp(pass, passwd) == 0) {
-                return _strdup(pass);
+        if (pos == length - 1) {
+            for (j = 0; j < length; ++j) {
+                pass[j] = pDict[indexes[j]];
             }
-
-			/* you may replace this function with the encryption function
-			   and return in same manner if the encryption string matches
-			   the one we are cracking */
-
-			for (position = x - 1; position > 0; --position) {
-				if (pass[position] == MAXCHAR) {
-                    memset(pass + position, MINCHAR, x - position);
-                    pass[position - 1]++;
-					found = 1;
-					break;
-				}
-			}
-
-			if (!found){
-				pass[x - 1]++;
-			}
-
-			count++;
-		}
-	}
-	//free(pass);
-	return NULL;
+            ++*attempts;
+            /* you may replace this function with the encryption function
+               and return in same manner if the encryption string matches
+               the one we are cracking */
+            if (strcmp(desired, pass) == 0) {
+                return 1;
+            }
+        } else {
+            if (MakeAttempt(pos + 1, length, pDict, indexes, pass, desired, attempts, maxIndex)) {
+                return 1;
+            }
+        }
+    }
+    return 0;
 }
 
+/*!
+* Caller must free memory allocated for the result in case of any
+*/
+char *BruteForce(int passmin, int passmax, const char *pDict, const char *desired, unsigned long long *attempts)
+{
+    char *pass = (char *)malloc(passmax + 1);
+    int *indexes = (int *)malloc(passmax * sizeof(int));
+    int passLength = passmin;
+    int maxIndex = strlen(pDict) - 1;
+
+    memset(pass, 0, passmax + 1);   // IMPORTANT
+
+    for (; passLength <= passmax; ++passLength) {
+        if (MakeAttempt(0, passLength, pDict, indexes, pass, desired, attempts, maxIndex)) {
+            goto cleanup;
+        }
+    }
+    // Nothing found
+    free(pass);
+    pass = NULL;
+cleanup:
+    free(indexes);
+    return pass;
+}
 
 int main(int argc, char *argv[])
 {
-	char *pass;
-	clock_t start, end;
-	double elapsed;
+    char *pass = NULL;
+    char *desired = NULL;   // it must be hash but it's plain string to demonstrate concept
+    clock_t start = 0;
+    clock_t end = 0;
+    double elapsed = 0.0;
+    unsigned long long count = 0;
 
-	if (argc != 2) {
-		printf("\nProof of concept brute force\nby Hi Tech Assassin\n");
-		printf("\nUsage: %s <pass to crack>\n\n", argv[0]);
-		return 0;
-	}
+    if (argc != 2) {
+        printf("\nProof of concept brute force\nby Alexander Egorov\n");
+        printf("\nUsage: %s <pass to crack>\n\n", argv[0]);
+        return 0;
+    }
 
-	start = clock();
-	passwd = argv[1];
-	printf("\nAttempting to brute force \"%s\"\n", passwd);
-	printf("Should take approximatelly around %.0f trys\n\n", pow((double)(MAXCHAR - MINCHAR), (int)strlen(passwd)));
+    start = clock();
+    desired = argv[1];
+    printf("\nAttempting to brute force \"%s\"\n", desired);
+    printf("Should take approximatelly around %.0f trys\n\n", pow((double)strlen(alphabet), (double)strlen(desired)));
 
-	if (pass = bruteforce(strlen(passwd), 1)) {
-		printf("The correct password is \"%s\"", pass);
-	} else {
-		printf("Hard luck\n");
-	}
+    if (pass = BruteForce(1, strlen(desired), alphabet, desired, &count)) {
+        printf("The correct password is \"%s\"", pass);
+        free(pass);
+    } else {
+        printf("Hard luck\n");
+    }
 
-	printf("\nNo of trys was %d\n", count);
-	end = clock();
-	elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
-	printf("Time elapsed = %f seconds\n", elapsed);
+    printf("\nNo of trys was %llu\n", count);
+    end = clock();
+    elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time elapsed = %.3f seconds\n", elapsed);
 
-	if (elapsed >= 1) {
-		printf("Trys per second was %f\n\n", count / elapsed);
-	} else {
-		printf("\n");
-	}
-
-	return 0;
+    if (elapsed >= 1) {
+        printf("Trys per second was %.3f\n", count / elapsed);
+    }
+    printf("\n");
+    return 0;
 }
+
--- src/bf/stdafx.h
+++ src/bf/stdafx.h
@@ -6,10 +6,3 @@
 #pragma once
 
 #include "targetver.h"
-
-#include <stdio.h>
-#include <tchar.h>
-
-
-
-// TODO: reference additional headers your program requires here
