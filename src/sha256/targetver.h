7af15181f50e5a963450dfa03575344771cc7057
egr <devnull@localhost>
2010-08-27 13:26:48 +0400

    targetver.h files made more VS2010 like :)

--- src/sha256/targetver.h
+++ src/sha256/targetver.h
@@ -9,23 +9,17 @@
  * Copyright: (c) Alexander Egorov 2009-2010
 */
 
-#ifndef SHA1_TARGETVER_H_
-#define SHA1_TARGETVER_H_
+#ifndef SHA256_TARGETVER_H_
+#define SHA256_TARGETVER_H_
 
-// The following macros define the minimum required platform.  The minimum required platform
-// is the earliest version of Windows, Internet Explorer etc. that has the necessary features to run 
-// your application.  The macros work by enabling all features available on platform versions up to and 
-// including the version specified.
+// Including SDKDDKVer.h defines the highest available Windows platform.
 
-// Modify the following defines if you have to target a platform prior to the ones specified below.
-// Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifdef WIN32
-#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows XP.
-#define _WIN32_WINNT 0x0501     // Change this to the appropriate value to target other versions of Windows.
-#endif
-#endif
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
 
 #define PRODUCT_VERSION "1.0.0.1"
 #define PROGRAM_NAME "sha256"
 
-#endif // SHA1_TARGETVER_H_
+#endif // SHA256_TARGETVER_H_
