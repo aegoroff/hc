3d0bdaff3d24f832e66af8f34d7ae2cf4b931f6e
egr <devnull@localhost>
2011-11-10 23:39:02 +0400

    feature: brute force wrapper all options implemented

--- bf/bf.py
+++ bf/bf.py
@@ -18,58 +18,44 @@ TOOLS = frozenset([
 # Use a shell for subcommands on Windows to get a PATH search.
 useShell = sys.platform.startswith("win")
 
-def RunShellCommand(command, printOutput=False,
-                    universalNewlines=True):
+def RunShellCommand(command, universalNewlines=True):
     """Executes a command and returns the output from stdout and the return code.
 
     Args:
       command: Command to execute.
-      printOutput: If True, the output is printed to stdout.
-                    If False, both stdout and stderr are ignored.
       universalNewlines: Use universal_newlines flag (default: True).
 
     Returns:
       Tuple (output, return code)
     """
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         shell=useShell, universal_newlines=universalNewlines)
-    if printOutput:
-        output_array = []
-        while True:
-            line = p.stdout.readline()
-            if not line:
-                break
-            print line.strip("\n")
-            output_array.append(line)
-        output = "".join(output_array)
-    else:
-        output = p.stdout.read()
+    p = subprocess.Popen(command, shell=useShell, universal_newlines=universalNewlines)
     p.wait()
-    errout = p.stderr.read()
-    if printOutput and errout:
-        print >> sys.stderr, errout
-    p.stdout.close()
-    p.stderr.close()
-    return output, p.returncode
+    return p.returncode
 
 
 def main():
     parser = argparse.ArgumentParser(description='Hash calculator')
     parser.add_argument('-t', '--algorithm', dest='algorithm', required=True,
                         help='Hash algorithm. Valid values crc32, md5, md4, sha1, sha256, sha384, sha512, whirlpool')
-    parser.add_argument('-s', '--hash', dest='hash', required=True, help='Hash string')
+    parser.add_argument('-s', '--hash', dest='hash', required=True, help='Hash string to crack')
     parser.add_argument('-n', '--min', dest='min', help='set minimum length of the string to restore. 1 by default')
     parser.add_argument('-x', '--max', dest='max', help='set maximum length of the string to restore. 10 by default')
-    parser.add_argument('-a', '--dict', dest='dict', help='initial string\'s dictionary by default all digits, upper and lower case latin symbols')
+    parser.add_argument('-a', '--dict', dest='dict',
+                        help='initial string\'s dictionary by default all digits, upper and lower case latin symbols')
 
     args = parser.parse_args()
 
+    cmd = ''
     if not len(TOOLS & {args.algorithm}):
         parser.error('Incorrect algorithm')
+    if args.min:
+        cmd += ' -n ' + args.min
+    if args.max:
+        cmd += ' -x ' + args.max
+    if args.dict:
+        cmd += ' -a ' + args.dict
 
-    data, retcode = RunShellCommand(['{0}.exe'.format(args.algorithm), "-c", '-m', args.hash], True)
-
-    return retcode
+    return RunShellCommand(['{0}.exe'.format(args.algorithm), "-c", '-m', args.hash, cmd])
 
 if __name__ == '__main__':
     sys.exit(main())
