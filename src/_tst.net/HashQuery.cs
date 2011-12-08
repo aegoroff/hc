483d00ba5acb86a2215deef4c4e9ad8388950568
egr <devnull@localhost>
2011-12-08 19:00:38 +0400

    part or several names matching test added. #HC-26 fixed

--- src/_tst.net/HashQuery.cs
+++ src/_tst.net/HashQuery.cs
@@ -405,6 +405,7 @@ namespace _tst.net
         [TestCase(1, "for file f from dir '{0}' where (f.name ~ '{1}' and (f.name ~ '{2}' or f.size == 0)) and f.path ~ '{3}' do {4} withsubs;", BaseTestDir, EmptyFileName, NotEmptyFileName, @".*sub.*")]
         [TestCase(0, "for file f from dir '{0}' where f.name ~ '' do {1} withsubs;", new object[] { BaseTestDir })]
         [TestCase(4, "for file f from dir '{0}' where f.name !~ '' do {1} withsubs;", new object[] { BaseTestDir })]
+        [TestCase(4, "for file f from dir '{0}' where f.name ~ 'mpty' do {1} withsubs;", new object[] { BaseTestDir })]
         public void CalcDir(int countResults, string template, params object[] parameters)
         {
             List<object> p = parameters.ToList();
