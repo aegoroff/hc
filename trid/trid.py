f834ff929e6bd09139b39cc1d448245b2a593e90
egoroff <egoroff@gmail.com>
2012-09-06 18:23:08 +0400

    extraction from single file completed

--- trid/trid.py
+++ trid/trid.py
@@ -26,19 +26,21 @@ def RunShellCommand(command, universalNewlines=True):
     p.wait()
     return p
 
-def main():
-    parser = argparse.ArgumentParser(description="TRiD signatures converting tool. Copyright (C) 2012 Alexander Egorov.")
-    parser.add_argument('-p', '--path', dest='path', help='Path to TRiD signature file', required=True)
 
-    args = parser.parse_args()
-
-
-    with open(args.path, 'r') as f:
+def extract(path):
+    title = ''
+    signature_ext = ''
+    descr = ''
+    bytes = ''
+    offset = 0
+    with open(path, 'r') as f:
+        list = []
         for event, element in etree.iterparse(f, events=("start", "end")):
-            #print("%5s, %4s, %s" % (event, element.tag, element.text))
             if event == 'start':
                 if element.tag == 'FileType':
                     title = element.text
+                if element.tag == 'Ext':
+                    signature_ext = element.text
                 if element.tag == 'Rem':
                     descr = element.text
                 if element.tag == 'Bytes':
@@ -50,17 +52,33 @@ def main():
                     binary = binascii.unhexlify(bytes)
                     tmp_file = "test.bin"
                     try:
-                        with open(tmp_file,"wb") as tmp:
+                        with open(tmp_file, "wb") as tmp:
                             tmp.write(binary)
                         p = RunShellCommand('md5 -f %s' % tmp_file)
                         s = p.stdout.read()
                         pieces = s.split('|')
                         hash = pieces[2].strip()
-                        print("%s: %i %s" % (bytes, offset, hash))
+                        list.append(
+                            "(f.offset == %i and f.limit == %i and f.md5 == '%s')" % (offset, len(bytes) / 2, hash))
                     finally:
                         os.remove(tmp_file)
+        where = ' and\n'.join(list)
+        if descr is None:
+            descr = ''
+        q = "# %s (%s)\n# %s\n\nfor file f from dir '.' where\n%s\ndo find;" % (title, signature_ext, descr, where)
+
+        dir, name = os.path.split(path)
+        root, ext = os.path.splitext(name)
+        with open("%s.hlq" % root, "w") as qf:
+            qf.write(q)
 
 
+def main():
+    parser = argparse.ArgumentParser(description="TRiD signatures converting tool. Copyright (C) 2012 Alexander Egorov.")
+    parser.add_argument('-p', '--path', dest='path', help='Path to TRiD signature file', required=True)
+
+    args = parser.parse_args()
+    extract(args.path)
 
 if __name__ == '__main__':
     sys.exit(main())
