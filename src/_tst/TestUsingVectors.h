fd01fdd468dff0051a9000d16f9b6ec46a585147
egr <egoroff@gmail.com>
2013-06-19 13:43:21 +0400

    not working MD2 test

--- src/_tst/TestUsingVectors.h
+++ src/_tst/TestUsingVectors.h
@@ -1,43 +1,57 @@
 #pragma once
 
+
+#define SZ_WHIRLPOOL    64
+#define SZ_SHA512       64
+#define SZ_SHA384       48
+#define SZ_RIPEMD320    40
+#define SZ_SHA256       32
+#define SZ_RIPEMD256    32
+#define SZ_SHA224       28
+#define SZ_TIGER192     24
+#define SZ_SHA1         20
+#define SZ_RIPEMD160    20
+#define SZ_RIPEMD128    16
+#define SZ_MD5          16
+#define SZ_MD4          16
+#define SZ_MD2          16
+
 #include "gtest.h"
-#include <stdio.h>
-#include <tchar.h>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <fstream>
+#include "apr.h"
+#include "apr_pools.h"
 using namespace std;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+static apr_pool_t* pool_;
+
 class TestUsingVectors : public ::testing::Test {
-    private:
-        vector<string> split(const char *str, char c = ' ')
-        {
-            vector<string> result;
-            do
-            {
-                const char *begin = str;
+    public:
+        bool CompareDigests(apr_byte_t* digest1, apr_byte_t* digest2, size_t sz);
 
-                while(*str != c && *str)
-                    str++;
-
-                result.push_back(string(begin, str));
-            } while (0 != *str++);
-
-            return result;
-        }
     protected:
+        void ToDigest(const char* hash, apr_byte_t* digest, size_t sz);
+        
         static void TearDownTestCase()
         {
+            apr_pool_destroy(pool_);
+            apr_terminate();
         }
 
         static void SetUpTestCase()
         {
-            ifstream infile("thefile.txt");
+            int argc = 1;
+
+            const char* const argv[] = { "1" };
+
+            apr_status_t status = apr_app_initialize(&argc, (const char *const **)&argv, NULL);
+
+            if (status != APR_SUCCESS) {
+                throw status;
+            }
+            apr_pool_create(&pool_, NULL);
         }
 };
 
