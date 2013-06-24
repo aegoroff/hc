3f83cabd5eddd3b431fdedea1468f3fc7b7afa06
egoroff <egoroff@gmail.com>
2013-06-24 19:25:54 +0400

    md4 from sphlib in HQ

---
+++ src/hlinq/md4def.h
@@ -0,0 +1,32 @@
+/*!
+ * \brief   The file contains MD4 API interface
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2011-11-21
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2013
+ */
+
+#ifndef MD4_HCALC_H_
+#define MD4_HCALC_H_
+
+#include "apr.h"
+#include "apr_errno.h"
+#include "..\md4\sph_md4.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+apr_status_t MD4CalculateDigest(apr_byte_t* digest, const void* input, const apr_size_t inputLen);
+apr_status_t MD4InitContext(void* context);
+apr_status_t MD4FinalHash(apr_byte_t* digest, void* context);
+apr_status_t MD4UpdateHash(void* context, const void* input, const apr_size_t inputLen);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MD4_HCALC_H_
