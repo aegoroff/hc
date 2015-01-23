3e73c3ceface0d2c7b0057c50c4aab2b9964c623
egoroff <egoroff@gmail.com>
2015-01-23 16:32:41 +0300

    non ASCII strings considered to be UTF-8 instead of ANSI so as to be more unversal

--- src/_tst.net/CmdStringTests.cs
+++ src/_tst.net/CmdStringTests.cs
@@ -138,7 +138,7 @@ namespace _tst.net
         public void CrackNonAsciiString()
         {
             Hash h = new Md5();
-            IList<string> results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, "55D6B444C5C95BD8DD6410D788422253", DictOpt, "еграб", MaxOpt, "3");
+            IList<string> results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, "327108899019B3BCFFF1683FBFDAF226", DictOpt, "еграб", MinOpt, "6", MaxOpt, "6");
             Assert.Equal(3, results.Count);
             Asserts.StringMatching(results[2], "Initial string is: *");
         }
