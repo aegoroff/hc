04cf384e89907ccc8591124d1dd0e675550e7fa5
egoroff <egoroff@gmail.com>
2015-05-05 08:23:18 +0300

    don't run Apc tests concurrently too

--- src/_tst.net/Apc.cs
+++ src/_tst.net/Apc.cs
@@ -39,6 +39,7 @@ egr4:$apr1$uths1zqo$4i/Rducjac63A.ExW4K6N1";
         }
     }
 
+    [Collection("FileTests")]
     public abstract class Apc<T> : ExeWrapper<T>, IClassFixture<ApcFixture> where T : Architecture, new()
     {
         private readonly string htpasswdPath;
