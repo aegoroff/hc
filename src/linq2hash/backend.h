337f97c6cbaccb071f45913712793fd8973fc8cd
egoroff <egoroff@gmail.com>
2015-10-09 00:37:09 +0300

    data source struct prototype

--- src/linq2hash/backend.h
+++ src/linq2hash/backend.h
@@ -13,6 +13,13 @@
 #define LINQ2HASH_BACKEND_H_
 #include "frontend.h"
 
+typedef struct bend_data_source_t {
+    type_def_t type;
+    char* name;
+    long long items_count;
+    char* (*get_item)(long long index);
+} bend_data_source_t;
+
 void bend_init(apr_pool_t* pool);
 void bend_cleanup();
 
