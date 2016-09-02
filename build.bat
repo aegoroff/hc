5b8c88b87b8fa3eb1b664ab34f65ccfea10c3206
egoroff <egoroff@gmail.com>
2016-09-02 20:30:32 +0300

    GnuBasePath pass using MSBUID parameter

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-setx GnuBasePath C:\Gnu
-msbuild src\linq2hash.sln /p:Configuration=Release
+msbuild src\linq2hash.sln /p:Configuration=Release;GnuBasePath=C:\Gnu
