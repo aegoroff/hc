86af57a8cf59885423ff3bfb524a8135daa61a11
egoroff <egoroff@gmail.com>
2016-09-10 00:25:30 +0300

    APC setup and doc removed

--- src/doc.gen/Program.cs
+++ src/doc.gen/Program.cs
@@ -11,24 +11,13 @@ using Antlr3.ST;
 
 namespace doc.gen
 {
-    internal class Program
+    internal static class Program
     {
         private static readonly IDictionary<string, string> hcNames = new Dictionary<string, string>
         {
             { "en", "Hash Calculator" },
             { "ru", "Хэш калькулятор" }
         };
-
-        #region Methods
-
-        private static void CreateApcDocumentationTxt(string docPath, string template, string lang)
-        {
-            var stringTemplate = new StringTemplate(File.ReadAllText(Path.Combine(docPath, template)));
-            stringTemplate.SetAttribute("exeName", "apc.exe");
-            stringTemplate.SetAttribute("appName", "Apache passwords cracker");
-            File.WriteAllText(Path.Combine(docPath, @"Readme.apc." + lang + ".txt"),
-                stringTemplate.ToString(), Encoding.UTF8);
-        }
 
         private static void CreateHqDocumentationTxt(string docPath, string template, string lang)
         {
@@ -42,18 +31,11 @@ namespace doc.gen
         private static void Main(string[] args)
         {
             if (args.Length == 0)
-            {
                 return;
-            }
-            string docPath = args[0];
-
-            CreateApcDocumentationTxt(docPath, @"Readme.Htpwdc.ru.st", "ru");
-            CreateApcDocumentationTxt(docPath, @"Readme.Htpwdc.en.st", "en");
+            var docPath = args[0];
 
             CreateHqDocumentationTxt(docPath, @"Readme.hc.ru.st", "ru");
             CreateHqDocumentationTxt(docPath, @"Readme.hc.en.st", "en");
         }
-
-        #endregion
     }
 }
