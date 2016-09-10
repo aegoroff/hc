652f0c3d467fd6727e9afb6fd9fde9c0da99d742
egoroff <egoroff@gmail.com>
2016-09-10 18:13:11 +0300

    empty string builtin

---
+++ src/hlinq/str.c
@@ -0,0 +1,13 @@
+/*!
+ * \brief   The file contains string builtin implementation
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2016-09-10
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2016
+ */
+
+#include "str.h"
+
---
+++ src/hlinq/str.h
@@ -0,0 +1,25 @@
+/*!
+ * \brief   The file contains string builtin interface
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2016-09-10
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2016
+ */
+
+#ifndef HLINQ_STR_H_
+#define HLINQ_STR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HLINQ_STR_H_
