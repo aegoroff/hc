296ee2e532b9597f1a891989c18840a04063276b
egoroff <egoroff@gmail.com>
2016-09-10 22:04:00 +0300

    hash and string command tests adopted

--- src/_tst.net/CmdStringTests.cs
+++ src/_tst.net/CmdStringTests.cs
@@ -18,13 +18,14 @@ namespace _tst.net
         private const string StringOpt = "-s";
         private const string LowCaseOpt = "-l";
         private const string NoProbeOpt = "--noprobe";
-        private const string CrackOpt = "-c";
         private const string HashOpt = "-m";
         private const string MaxOpt = "-x";
         private const string MinOpt = "-n";
         private const string DictOpt = "-a";
         private const string PerfOpt = "-p";
         private const string EmptyStr = "\"\"";
+        private const string StringCmd = "string";
+        private const string HashCmd = "hash";
 
 
         protected CmdStringTests() : base(new T())
@@ -33,50 +34,50 @@ namespace _tst.net
 
         protected override IList<string> RunEmptyStringCrack(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.EmptyStringHash);
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.EmptyStringHash);
         }
 
         protected override IList<string> RunStringCrack(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3");
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3");
         }
 
         protected override IList<string> RunStringCrackTooShort(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString, MaxOpt,
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.HashString, MaxOpt,
                                                 (h.InitialString.Length - 1).ToString(), DictOpt, h.InitialString);
         }
 
         protected override IList<string> RunStringCrackTooMinLength(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString, MinOpt,
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.HashString, MinOpt,
                                                 (h.InitialString.Length + 1).ToString(), MaxOpt,
                                                 (h.InitialString.Length + 2).ToString(), DictOpt, h.InitialString);
         }
 
         protected override IList<string> RunStringCrackLowCaseHash(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString.ToLowerInvariant(), MaxOpt, "3", DictOpt, h.InitialString);
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.HashString.ToLowerInvariant(), MaxOpt, "3", DictOpt, h.InitialString);
         }
 
         protected override IList<string> RunStringHash(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, StringOpt, h.InitialString);
+            return this.Runner.Run(h.Algorithm, StringCmd, StringOpt, h.InitialString);
         }
 
         protected override IList<string> RunStringHashLowCase(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, StringOpt, h.InitialString, LowCaseOpt);
+            return this.Runner.Run(h.Algorithm, StringCmd, StringOpt, h.InitialString, LowCaseOpt);
         }
 
         protected override IList<string> RunEmptyStringHash(Hash h)
         {
-            return this.Runner.Run(h.Algorithm, StringOpt, EmptyStr);
+            return this.Runner.Run(h.Algorithm, StringCmd, StringOpt, EmptyStr);
         }
 
         protected override IList<string> RunCrackStringUsingNonDefaultDictionary(Hash h, string dict)
         {
-            return this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.StartPartStringHash, DictOpt, dict, MaxOpt, "2", MinOpt, "2");
+            return this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.StartPartStringHash, DictOpt, dict, MaxOpt, "2", MinOpt, "2");
         }
 
         private static byte[] StringToByteArray(string hex)
@@ -97,7 +98,7 @@ namespace _tst.net
             var base64 = Convert.ToBase64String(bytes);
             
             // Act
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, "-b", base64, MaxOpt, "3");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, "-b", HashOpt, base64, MaxOpt, "3");
 
             // Assert
             results[1].Should().Be(string.Format(RestoredStringTemplate, h.InitialString), $"Because {base64} must be restored to {h.InitialString}");
@@ -109,7 +110,7 @@ namespace _tst.net
         public void CrackString_SingleThread_Success(Hash h)
         {
             // Act
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3", "-T", "1");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, HashOpt, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3", "-T", "1");
 
             // Assert
             results[1].Should().Be(string.Format(RestoredStringTemplate, h.InitialString));
@@ -121,7 +122,7 @@ namespace _tst.net
         public void CrackString_BadThreadCountNumber_Success(Hash h, string threads)
         {
             // Act
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3", "-T", threads);
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.HashString, MaxOpt, "3", "-T", threads);
 
             // Assert
             results[2].Should().Be(string.Format(RestoredStringTemplate, h.InitialString));
@@ -135,7 +136,7 @@ namespace _tst.net
         public void CrackString_SingleCharStringWithMaxOpt_Success(Hash h)
         {
             // Act
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2");
 
             // Assert
             results[1].Should().Be(string.Format(RestoredStringTemplate, "2"));
@@ -146,7 +147,7 @@ namespace _tst.net
         [Theory, MemberData(nameof(Hashes))]
         public void CrackStringSingleCharStringWithMaxOptOnSingleThread(Hash h)
         {
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2", "-T", "1");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2", "-T", "1");
             Assert.Equal(string.Format(RestoredStringTemplate, "2"), results[1]);
             Assert.Equal(2, results.Count);
         }
@@ -155,7 +156,7 @@ namespace _tst.net
         [Theory, MemberData(nameof(Hashes))]
         public void CrackStringSingleCharStringWithMaxOptAndNonDefaultDict(Hash h)
         {
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2", DictOpt, "[0-9]");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, h.MiddlePartStringHash, MaxOpt, "2", DictOpt, "[0-9]");
             Assert.Equal(string.Format(RestoredStringTemplate, "2"), results[1]);
             Assert.Equal(2, results.Count);
         }
@@ -164,7 +165,7 @@ namespace _tst.net
         [Theory, MemberData(nameof(Hashes))]
         public void TestPerformance(Hash h)
         {
-            var results = this.Runner.Run(h.Algorithm, PerfOpt, DictOpt, "12345", MaxOpt, "5", MinOpt, "5");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, PerfOpt, DictOpt, "12345", MaxOpt, "5", MinOpt, "5");
             Assert.Equal(string.Format(RestoredStringTemplate, "12345"), results[2]);
             Assert.Equal(3, results.Count);
         }
@@ -174,7 +175,7 @@ namespace _tst.net
         public void CrackNonAsciiString()
         {
             Hash h = new Md5();
-            var results = this.Runner.Run(h.Algorithm, CrackOpt, NoProbeOpt, HashOpt, "327108899019B3BCFFF1683FBFDAF226", DictOpt, "еграб", MinOpt, "6", MaxOpt, "6");
+            var results = this.Runner.Run(h.Algorithm, HashCmd, NoProbeOpt, HashOpt, "327108899019B3BCFFF1683FBFDAF226", DictOpt, "еграб", MinOpt, "6", MaxOpt, "6");
             Asserts.StringMatching(results[1], "Initial string is: *");
             Assert.Equal(2, results.Count);
         }
