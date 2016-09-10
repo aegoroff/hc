63b71b80c93f132597d08227945719bd66581229
egoroff <egoroff@gmail.com>
2016-09-10 18:16:17 +0300

    common builtins code draft

---
+++ src/hlinq/builtin.c
@@ -0,0 +1,12 @@
+/*!
+ * \brief   The file contains common builtin implementation
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2016-09-10
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2016
+ */
+
+#include "builtin.h"
---
+++ src/hlinq/builtin.h
@@ -0,0 +1,25 @@
+/*!
+ * \brief   The file contains commit builtins interface
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2016-09-10
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2016
+ */
+
+#ifndef HLINQ_BUILTIN_H_
+#define HLINQ_BUILTIN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HLINQ_BUILTIN_H_
