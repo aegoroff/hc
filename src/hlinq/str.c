b14d4a8f3c737c6c1a4d6879b720a9c641b3b6b3
egoroff <egoroff@gmail.com>
2016-09-10 19:46:11 +0300

    unnecessary casting removed. p renamed to pool to improve code readability

--- src/hlinq/str.c
+++ src/hlinq/str.c
@@ -16,21 +16,21 @@ void str_run(string_builtin_ctx_t* ctx) {
     apr_byte_t* digest;
     apr_size_t sz;
     out_context_t o = { 0 };
-    apr_pool_t* p;
+    apr_pool_t* pool;
     hash_definition_t* hash;
     builtin_ctx_t* builtin_ctx;
 
     builtin_ctx = ctx->builtin_ctx_;
 
-    p = builtin_get_pool();
+    pool = builtin_get_pool();
     hash = builtin_get_hash_definition();
     sz = hash->hash_length_;
     
-    digest = (apr_byte_t*)apr_pcalloc(p, sizeof(apr_byte_t) * sz);
+    digest = apr_pcalloc(pool, sizeof(apr_byte_t) * sz);
 
     // some hashes like NTLM required unicode string so convert multi byte string to unicode one
     if (hash->use_wide_string_) {
-        wchar_t* str = enc_from_ansi_to_unicode(ctx->string_, p);
+        wchar_t* str = enc_from_ansi_to_unicode(ctx->string_, pool);
         hash->pfn_digest_(digest, str, wcslen(str) * sizeof(wchar_t));
     }
     else {
@@ -39,6 +39,6 @@ void str_run(string_builtin_ctx_t* ctx) {
 
     o.is_finish_line_ = TRUE;
     o.is_print_separator_ = FALSE;
-    o.string_to_print_ = out_hash_to_string(digest, builtin_ctx->is_print_low_case_, sz, p);
+    o.string_to_print_ = out_hash_to_string(digest, builtin_ctx->is_print_low_case_, sz, pool);
     builtin_ctx->pfn_output_(&o);
 }
