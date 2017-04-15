a846faa8ffa6cd855770133c7190cc10ff9fa4d1
egoroff <egoroff@gmail.com>
2017-04-15 20:43:41 +0300

    file headers added

--- src/_tst/ArgtableFileTest.cpp
+++ src/_tst/ArgtableFileTest.cpp
@@ -1,4 +1,19 @@
-﻿#include "ArgtableFileTest.h"
+﻿/*
+* This is an open source non-commercial project. Dear PVS-Studio, please check it.
+* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+*/
+/*!
+ * \brief   The file contains argtable test implementation
+ * \author  \verbatim
+            Created by: Alexander Egorov
+            \endverbatim
+ * \date    \verbatim
+            Creation date: 2017-04-15
+            \endverbatim
+ * Copyright: (c) Alexander Egorov 2009-2017
+ */
+
+#include "ArgtableFileTest.h"
 #include "argtable3.h"
 
 void ArgtableFileTest::SetUp() {
