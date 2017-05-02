4e4efaa36fa16578bb1a081a1cda346e57b0a64f
Alexander Egorov <alegorov@luxoft.com>
2017-05-02 17:29:56 +0300

    string replace draft

--- src/srclib/bf.c
+++ src/srclib/bf.c
@@ -56,6 +56,7 @@ static void* APR_THREAD_FUNC prbf_make_attempt_thread_func(apr_thread_t* thd, vo
 static char* prbf_commify(char* numstr, apr_pool_t* pool);
 static char* prbf_double_to_string(double value, apr_pool_t* pool);
 static char* prbf_int64_to_string(uint64_t value, apr_pool_t* pool);
+static char* prbf_str_replace(char* orig, char* rep, char* with, apr_pool_t* pool);
 
 void bf_crack_hash(const char* dict,
                    const char* hash,
@@ -365,4 +366,50 @@ char* prbf_commify(char* numstr, apr_pool_t* pool) {
     // ReSharper disable once CppPossiblyErroneousEmptyStatements
     while(*numstr++ = *wk++);
     return _strrev(ret);
+}
+
+char* prbf_str_replace(char* orig, char* rep, char* with, apr_pool_t* pool) {
+    char* result; // the return string
+    char* ins; // the next insert point
+    char* tmp; // varies
+    int len_rep; // length of rep (the string to remove)
+    int len_with; // length of with (the string to replace rep with)
+    int len_front; // distance between rep and end of last rep
+    int count; // number of replacements
+
+    // sanity checks and initialization
+    if(!orig || !rep)
+        return NULL;
+    len_rep = strlen(rep);
+    if(len_rep == 0)
+        return orig;
+    if(!with)
+        with = "";
+    len_with = strlen(with);
+
+    // count the number of replacements needed
+    ins = orig;
+    for(count = 0; tmp = strstr(ins, rep); ++count) {
+        ins = tmp + len_rep;
+    }
+
+    tmp = result = (char*)apr_pcalloc(pool, strlen(orig) + (len_with - len_rep) * count + 1);
+
+    if(!result)
+        return orig;
+
+    // first time through the loop, all the variable are set correctly
+    // from here on,
+    //    tmp points to the end of the result string
+    //    ins points to the next occurrence of rep in orig
+    //    orig points to the remainder of orig after "end of rep"
+    while(count--) {
+        ins = strstr(orig, rep);
+        len_front = ins - orig;
+        tmp = strncpy(tmp, orig, len_front) + len_front;
+        tmp = strcpy(tmp, with) + len_with;
+        orig += len_front + len_rep; // move to next "end of rep"
+    }
+    strcpy(tmp, orig);
+    return result;
 }
