d1d888fb49fff495979dd1f86538e430cca0369b
egoroff <egoroff@gmail.com>
2017-09-27 10:59:56 +0300

    code reformatting

--- src/hc/gpu.cu
+++ src/hc/gpu.cu
@@ -16,21 +16,20 @@
 #include "cuda_runtime.h"
 #include "gpu.h"
 
-void gpu_get_props(device_props_t* prop)
-{
+void gpu_get_props(device_props_t* prop) {
     cudaDeviceProp device_prop;
     int n_dev_count;
 
-    cudaGetDeviceCount(&n_dev_count);
+    if(cudaSuccess != cudaGetDeviceCount(&n_dev_count)) {
+        return;
+    }
 
     prop->device_count = n_dev_count;
     prop->max_blocks_number = 0;
     prop->max_threads_per_block = 0;
 
-    for (int i = 0; i < n_dev_count; i++)
-    {
-        if (cudaSuccess != cudaGetDeviceProperties(&device_prop, i))
-        {
+    for(int i = 0; i < n_dev_count; i++) {
+        if(cudaSuccess != cudaGetDeviceProperties(&device_prop, i)) {
             prop->max_blocks_number += 64;
             prop->max_threads_per_block += 128;
             return;
