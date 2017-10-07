c9a1f6116d2b22f97ed2fed75faf37c4f3cccf63
egoroff <egoroff@gmail.com>
2017-10-07 11:58:51 +0300

    CUDA functions prefixes to improve code readability

--- src/hc/sha1.cu
+++ src/hc/sha1.cu
@@ -61,18 +61,18 @@ __constant__ unsigned char k_hash[DIGESTSIZE];
 
 __global__ void sha1_kernel(unsigned char* result, unsigned char* variants, const uint32_t attempt_length, const uint32_t dict_length);
 
-void sha1_on_gpu_prepare(int device_ix, const unsigned char* dict, size_t dict_len, const unsigned char* hash, char** variants, size_t variants_len) {
+__host__ void sha1_on_gpu_prepare(int device_ix, const unsigned char* dict, size_t dict_len, const unsigned char* hash, char** variants, size_t variants_len) {
     CUDA_SAFE_CALL(cudaSetDevice(device_ix));
     CUDA_SAFE_CALL(cudaMemcpyToSymbol(k_dict, dict, dict_len * sizeof(char)));
     CUDA_SAFE_CALL(cudaMemcpyToSymbol(k_hash, hash, DIGESTSIZE));
     CUDA_SAFE_CALL(cudaHostAlloc(reinterpret_cast<void**>(variants), variants_len * sizeof(char), cudaHostAllocDefault));
 }
 
-void sha1_on_gpu_cleanup(gpu_tread_ctx_t* ctx) {
+__host__ void sha1_on_gpu_cleanup(gpu_tread_ctx_t* ctx) {
     CUDA_SAFE_CALL(cudaFreeHost(ctx->variants_));
 }
 
-void sha1_run_on_gpu(gpu_tread_ctx_t* ctx, size_t dict_len, unsigned char* variants, const size_t variants_size) {
+__host__ void sha1_run_on_gpu(gpu_tread_ctx_t* ctx, size_t dict_len, unsigned char* variants, const size_t variants_size) {
     unsigned char* dev_result = nullptr;
     unsigned char* dev_variants = nullptr;
 
