16bd09fd5e83a9795d26516e45b0262462fd3b1f
egoroff <egoroff@gmail.com>
2019-08-09 15:56:24 +0300

    gitlab ci/cd experiments

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
-msbuild src\linq2hash.sln /p:Configuration=Release;GnuBasePath=C:\Gnu
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-msbuild src\linq2hash.sln /p:Configuration=Release;Platform=Win32;GnuBasePath=C:\Gnu
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+msbuild hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu
 src\x64\Release\_tst.exe
 src\Release\_tst.exe
