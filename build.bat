2ac857db3d8975c95530106b93150d5a1f04c155
egoroff <egoroff@gmail.com>
2019-08-09 17:08:25 +0300

    Build using x64

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-msbuild src\hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu
+msbuild src\hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu;Platform=x64
 src\x64\Release\_tst.exe
 src\Release\_tst.exe
