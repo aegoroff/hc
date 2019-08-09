be6254a54561b719c46450a7b769d59e9bb47992
egoroff <egoroff@gmail.com>
2019-08-09 21:20:35 +0300

    Diag verbosity changed to detailed

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-msbuild src\hc.xml /p:Configuration=Release /verbosity:diag
+msbuild src\hc.xml /p:Configuration=Release /verbosity:detailed
