835f5ecfe151b8a26071be0dffbbf31cf946dd61
egoroff <egoroff@gmail.com>
2019-08-09 16:04:14 +0300

    Path to project file fixed

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-msbuild hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu
+msbuild src\hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu
 src\x64\Release\_tst.exe
 src\Release\_tst.exe
