7a4175f9ff591a5661f52444cab6ed7f68d85803
egoroff <egoroff@gmail.com>
2019-08-09 17:16:06 +0300

    trying to run x64 build under GitLab

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
-msbuild src\hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu;Platform=x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+msbuild src\hc.xml /p:Configuration=Release;GnuBasePath=C:\Gnu
 src\x64\Release\_tst.exe
 src\Release\_tst.exe
