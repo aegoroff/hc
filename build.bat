94936c16f0a214f17a267acbcf21af0f19d683e4
egoroff <egoroff@gmail.com>
2019-08-09 20:59:21 +0300

    verbosity=diagnostic on build

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-msbuild src\hc.xml /p:Configuration=Release
+msbuild src\hc.xml /p:Configuration=Release;verbosity=diagnostic
