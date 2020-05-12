6dde62fc9592bbb3d72e6deb26d2895dd265ef3c
egoroff <egoroff@gmail.com>
2020-05-12 10:36:22 +0300

    encoding fixes + unit tests

--- src/srclib/encoding.c
+++ src/srclib/encoding.c
@@ -1,7 +1,3 @@
-/*
-* This is an open source non-commercial project. Dear PVS-Studio, please check it.
-* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-*/
 /*!
  * \brief   The file contains encoding functions implementation
  * \author  \verbatim
@@ -10,7 +6,7 @@
  * \date    \verbatim
             Creation date: 2011-03-06
             \endverbatim
- * Copyright: (c) Alexander Egorov 2009-2019
+ * Copyright: (c) Alexander Egorov 2009-2020
  */
 
 #include <stdio.h>
@@ -21,6 +17,35 @@
 #ifndef WIN32
 #include <stdlib.h>
 #endif
+
+typedef struct bom_def {
+    bom_t bom;
+    size_t length;
+    unsigned char signature[BOM_MAX_LEN];
+} bom_def_t;
+
+static char* prenc_from_unicode_to_code_page(const wchar_t* from, UINT code_page, apr_pool_t* pool);
+
+static const char* enc_bom_names[] = {
+    "Unknown", "UTF-8", "UTF-16 (LE)", "UTF-16 (BE)", "UTF-32 (BE)",
+};
+
+static bom_def_t boms[] = {
+    // Various UTF encodings
+    { bom_utf8, 3, { 0xEF, 0xBB, 0xBF } },          // UTF8
+    { bom_utf16le, 2, { 0xFF, 0xFE } },             // UTF16LE
+    { bom_utf16be, 2, { 0xFE, 0xFF } },             // UTF16BE
+    { bom_utf32be, 4, { 0x00, 0x00, 0xFE, 0xFF } }, // UTF32BE
+    // Add others as desired.  https://en.wikipedia.org/wiki/Byte_order_mark
+    { bom_unknown, 0, { 0 } }
+};
+
+const char* enc_get_encoding_name(bom_t bom) {
+    if(bom < 0 || bom > bom_utf32be - 1) {
+        return NULL;
+    }
+    return enc_bom_names[bom];
+}
 
 /*!
  * IMPORTANT: Memory allocated for result must be freed up by caller
@@ -49,17 +74,28 @@ char* enc_from_ansi_to_utf8(const char* from, apr_pool_t* pool) {
 */
 wchar_t* enc_from_ansi_to_unicode(const char* from, apr_pool_t* pool) {
 #ifdef WIN32
-    const size_t cb_from = strlen(from) + 1;
-    // IMPORTANT!!! including null terminator
-    const int length_wide = MultiByteToWideChar(CP_ACP, 0, from, (int)cb_from, NULL, 0);
+
+    /**
+     * cbMultiByte
+     * Size, in bytes, of the string indicated by the lpMultiByteStr parameter.
+     * Alternatively, this parameter can be set to -1 if the string is null-terminated. Note that, if cbMultiByte is 0, the function fails.
+     * If this parameter is -1, the function processes the entire input string, including the terminating null character.
+     * Therefore, the resulting Unicode string has a terminating null character, and the length returned by the function includes this character.
+     * If this parameter is set to a positive integer, the function processes exactly the specified number of bytes.
+     * If the provided size does not include a terminating null character, the resulting Unicode string is not null-terminated
+     * and the returned length does not include this character.
+     */
+    const int multi_byte_size = -1;
+    
+    const int length_wide = MultiByteToWideChar(CP_ACP, 0, from, multi_byte_size, NULL, 0);
     // including null terminator
     const apr_size_t wide_buffer_size = sizeof(wchar_t) * (apr_size_t)length_wide;
-    wchar_t* wide_str                 = (wchar_t*)apr_pcalloc(pool, wide_buffer_size);
+    wchar_t* wide_str = (wchar_t*)apr_pcalloc(pool, wide_buffer_size);
     if(wide_str == NULL) {
         lib_printf(ALLOCATION_FAILURE_MESSAGE, wide_buffer_size, __FILE__, __LINE__);
         return NULL;
     }
-    MultiByteToWideChar(CP_ACP, 0, from, (int)cb_from, wide_str, length_wide);
+    MultiByteToWideChar(CP_ACP, 0, from, multi_byte_size, wide_str, length_wide);
     return wide_str;
 #else
     wchar_t* result = NULL;
@@ -74,17 +110,129 @@ wchar_t* enc_from_ansi_to_unicode(const char* from, apr_pool_t* pool) {
 * IMPORTANT: Memory allocated for result must be freed up by caller
 */
 char* enc_from_unicode_to_ansi(const wchar_t* from, apr_pool_t* pool) {
+    return prenc_from_unicode_to_code_page(from, CP_ACP, pool);
+}
+
+/*!
+* IMPORTANT: Memory allocated for result must be freed up by caller
+*/
+char* enc_from_unicode_to_utf8(const wchar_t* from, apr_pool_t* pool) {
+    return prenc_from_unicode_to_code_page(from, CP_UTF8, pool);
+}
+
+BOOL enc_is_valid_utf8(const char* str) {
+    if(!str) {
+        return FALSE;
+    }
+
+    const unsigned char* bytes = (const unsigned char*)str;
+    unsigned int cp;
+    int num;
+
+    while(*bytes != 0x00) {
+        if((*bytes & 0x80) == 0x00) {
+            // U+0000 to U+007F
+            cp = (*bytes & 0x7F);
+            num = 1;
+        } else if((*bytes & 0xE0) == 0xC0) {
+            // U+0080 to U+07FF
+            cp = (*bytes & 0x1F);
+            num = 2;
+        } else if((*bytes & 0xF0) == 0xE0) {
+            // U+0800 to U+FFFF
+            cp = (*bytes & 0x0F);
+            num = 3;
+        } else if((*bytes & 0xF8) == 0xF0) {
+            // U+10000 to U+10FFFF
+            cp = (*bytes & 0x07);
+            num = 4;
+        } else {
+            return FALSE;
+        }
+
+        bytes += 1;
+        for(int i = 1; i < num; ++i) {
+            if((*bytes & 0xC0) != 0x80) {
+                return FALSE;
+            }
+            cp = (cp << 6) | (*bytes & 0x3F);
+            bytes += 1;
+        }
+
+        if((cp > 0x10FFFF) ||
+            ((cp >= 0xD800) && (cp <= 0xDFFF)) ||
+            ((cp <= 0x007F) && (num != 1)) ||
+            ((cp >= 0x0080) && (cp <= 0x07FF) && (num != 2)) ||
+            ((cp >= 0x0800) && (cp <= 0xFFFF) && (num != 3)) ||
+            ((cp >= 0x10000) && (cp <= 0x1FFFFF) && (num != 4))) {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+bom_t enc_detect_bom(apr_file_t* f) {
+    unsigned char bom_signature[BOM_MAX_LEN];
+    apr_off_t apr_offset = 0;
+    apr_status_t status = apr_file_seek(f, APR_SET, &apr_offset); // Only file beginning
+    if(status != APR_SUCCESS) {
+        return bom_unknown;
+    }
+
+    apr_size_t nbytes = BOM_MAX_LEN;
+    status = apr_file_read(f, bom_signature, &nbytes);
+    if(status != APR_SUCCESS) {
+        return bom_unknown;
+    }
+
+    size_t offset = 0;
+    bom_t result = enc_detect_bom_memory(bom_signature, nbytes, &offset);
+
+    apr_offset = (apr_off_t)offset;
+    apr_file_seek(f, APR_SET, &apr_offset); // Leave file position to just after BOM
+
+    return result;
+}
+
+bom_t enc_detect_bom_memory(const unsigned char* buffer, size_t len, size_t* offset) {
+    for(size_t i = 0; boms[i].length; i++) {
+        if(len >= boms[i].length && memcmp(buffer, boms[i].signature, boms[i].length) == 0) {
+            *offset = boms[i].length;
+            return boms[i].bom;
+        }
+    }
+    return bom_unknown;
+}
+
+/*!
+* IMPORTANT: Memory allocated for result must be freed up by caller
+*/
+char* prenc_from_unicode_to_code_page(const wchar_t* from, UINT code_page, apr_pool_t* pool) {
 #ifdef WIN32
-    char* ansi_str        = NULL;
-    const int length_ansi = WideCharToMultiByte(CP_ACP, 0, from, wcslen(from), ansi_str, 0, NULL, NULL);
+    char* ansi_str = NULL;
+
+    /**
+     * cchWideChar
+     * Size, in characters, of the string indicated by lpWideCharStr.
+     * Alternatively, this parameter can be set to -1 if the string is null-terminated. If cchWideChar is set to 0, the function fails.
+     * If this parameter is -1, the function processes the entire input string, including the terminating null character.
+     * Therefore, the resulting character string has a terminating null character, and the length returned by the function includes this character.
+     * If this parameter is set to a positive integer, the function processes exactly the specified number of characters.
+     * If the provided size does not include a terminating null character, the resulting character
+     * string is not null-terminated, and the returned length does not include this character.
+     */
+    const int wide_size = -1;
+
+    const int length_ansi = WideCharToMultiByte(code_page, 0, from, wide_size, ansi_str, 0, NULL, NULL);
     // null terminator included
-    ansi_str = (char*)apr_pcalloc(pool, (apr_size_t)((apr_size_t)length_ansi + 1));
+    ansi_str = (char*)apr_pcalloc(pool, (apr_size_t)((apr_size_t)length_ansi));
 
     if(ansi_str == NULL) {
         lib_printf(ALLOCATION_FAILURE_MESSAGE, length_ansi, __FILE__, __LINE__);
         return NULL;
     }
-    WideCharToMultiByte(CP_ACP, 0, from, wcslen(from), ansi_str, length_ansi, NULL, NULL);
+    WideCharToMultiByte(code_page, 0, from, wide_size, ansi_str, length_ansi, NULL, NULL);
 
     return ansi_str;
 #else
@@ -97,21 +245,33 @@ char* enc_from_unicode_to_ansi(const wchar_t* from, apr_pool_t* pool) {
 }
 
 #ifdef WIN32
+
 /*!
  * IMPORTANT: Memory allocated for result must be freed up by caller
  */
 char* enc_decode_utf8_ansi(const char* from, UINT from_code_page, UINT to_code_page, apr_pool_t* pool) {
-    char* ansi_str        = NULL;
-    const size_t cb_from  = strlen(from) + 1; // IMPORTANT!!! including null terminator
-    const int length_wide = MultiByteToWideChar(from_code_page, 0, from, (int)cb_from, NULL, 0);
-    // including null terminator
+    char* ansi_str = NULL;
+    /**
+     * cbMultiByte
+     * Size, in bytes, of the string indicated by the lpMultiByteStr parameter.
+     * Alternatively, this parameter can be set to -1 if the string is null-terminated. Note that, if cbMultiByte is 0, the function fails.
+     * If this parameter is -1, the function processes the entire input string, including the terminating null character.
+     * Therefore, the resulting Unicode string has a terminating null character, and the length returned by the function includes this character.
+     * If this parameter is set to a positive integer, the function processes exactly the specified number of bytes.
+     * If the provided size does not include a terminating null character, the resulting Unicode string is not null-terminated
+     * and the returned length does not include this character.
+     */
+    const int multi_byte_size = -1;
+
+    const int length_wide = MultiByteToWideChar(from_code_page, 0, from, multi_byte_size, NULL, 0);
+
     const apr_size_t wide_buffer_size = sizeof(wchar_t) * (apr_size_t)length_wide;
-    wchar_t* wide_str                 = (wchar_t*)apr_pcalloc(pool, wide_buffer_size);
+    wchar_t* wide_str = (wchar_t*)apr_pcalloc(pool, wide_buffer_size);
     if(wide_str == NULL) {
         lib_printf(ALLOCATION_FAILURE_MESSAGE, wide_buffer_size, __FILE__, __LINE__);
         return NULL;
     }
-    MultiByteToWideChar(from_code_page, 0, from, (int)cb_from, wide_str, length_wide);
+    MultiByteToWideChar(from_code_page, 0, from, multi_byte_size, wide_str, length_wide);
 
     const int length_ansi = WideCharToMultiByte(to_code_page, 0, wide_str, length_wide, ansi_str, 0, NULL, NULL);
     // null terminator included
@@ -125,4 +285,5 @@ char* enc_decode_utf8_ansi(const char* from, UINT from_code_page, UINT to_code_p
 
     return ansi_str;
 }
+
 #endif
--- src/srclib/encoding.h
+++ src/srclib/encoding.h
@@ -1,7 +1,3 @@
-/*
-* This is an open source non-commercial project. Dear PVS-Studio, please check it.
-* PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-*/
 /*!
  * \brief   The file contains encoding functions interface
  * \author  \verbatim
@@ -10,17 +6,28 @@
  * \date    \verbatim
             Creation date: 2011-03-06
             \endverbatim
- * Copyright: (c) Alexander Egorov 2009-2019
+ * Copyright: (c) Alexander Egorov 2009-2020
  */
 
-#ifndef LINQ2HASH_ENCODING_H_
-#define LINQ2HASH_ENCODING_H_
+#ifndef PCTRL_ENCODING_H_
+#define PCTRL_ENCODING_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include "apr_pools.h"
+#include "apr_file_io.h"
+
+typedef enum {
+    bom_unknown = 0,
+    bom_utf8 = 1,
+    bom_utf16le = 2,
+    bom_utf16be = 3,
+    bom_utf32be = 4
+} bom_t;
+
+#define BOM_MAX_LEN 5
 
 /*!
  * IMPORTANT: Memory allocated for result must be freed up by caller
@@ -42,15 +49,29 @@ wchar_t* enc_from_ansi_to_unicode(const char* from, apr_pool_t* pool);
  */
 char* enc_from_unicode_to_ansi(const wchar_t* from, apr_pool_t* pool);
 
+/*!
+ * IMPORTANT: Memory allocated for result must be freed up by caller
+ */
+char* enc_from_unicode_to_utf8(const wchar_t* from, apr_pool_t* pool);
+
+BOOL enc_is_valid_utf8(const char* str);
+
+bom_t enc_detect_bom(apr_file_t* f);
+bom_t enc_detect_bom_memory(const unsigned char* buffer, size_t len, size_t* offset);
+
+const char* enc_get_encoding_name(bom_t bom);
+
 #ifdef WIN32
+
 /*!
  * IMPORTANT: Memory allocated for result must be freed up by caller
  */
 char* enc_decode_utf8_ansi(const char* from, UINT from_code_page, UINT to_code_page, apr_pool_t* pool);
+
 #endif
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // LINQ2HASH_ENCODING_H_
+#endif // PCTRL_ENCODING_H_
