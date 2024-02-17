ce9725689cd0fc6b1a4cf54ce347d829591c1fa3
Alexander Egorov <egoroff@gmail.com>
2024-02-17 17:01:34 +0300

    feat: bump PCRE2 to 10.43

--- linux_build.sh
+++ linux_build.sh
@@ -13,6 +13,7 @@ APR_SRC=apr-1.7.4
 APR_UTIL_SRC=apr-util-1.6.3
 EXPAT_SRC=expat-2.5.0
 OPENSSL_SRC=openssl-3.2.1
+PCRE_SRC=pcre2-10.43
 
 [[ -d "${LIB_INSTALL_SRC}" ]] || mkdir -p ${LIB_INSTALL_SRC}
 #[[ -d "${LIB_INSTALL_PREFIX}" ]] && rm -rf ${LIB_INSTALL_PREFIX}
@@ -22,11 +23,13 @@ rm -rf ${LIB_INSTALL_SRC}/${EXPAT_SRC}
 rm -rf ${LIB_INSTALL_SRC}/${APR_SRC}
 rm -rf ${LIB_INSTALL_SRC}/${APR_UTIL_SRC}
 rm -rf ${LIB_INSTALL_SRC}/${OPENSSL_SRC}
+rm -rf ${LIB_INSTALL_SRC}/${PCRE_SRC}
 
 EXTERNAL_PREFIX=$(realpath ${LIB_INSTALL_PREFIX})
 EXPAT_PREFIX=${EXTERNAL_PREFIX}/expat
 APR_PREFIX=${EXTERNAL_PREFIX}/apr
 OPENSSL_PREFIX=${EXTERNAL_PREFIX}/openssl
+PCRE_PREFIX=${EXTERNAL_PREFIX}/pcre
 
 if [[ "${ARCH}" = "x86_64" ]]; then
 	CFLAGS="-Ofast -march=haswell -mtune=haswell"
@@ -58,6 +61,12 @@ if [[ ! -d "${APR_PREFIX}" ]]; then
 	(cd ${LIB_INSTALL_SRC} && [[ -f "${APR_UTIL_SRC}.tar.gz" ]] || wget https://dlcdn.apache.org/apr/${APR_UTIL_SRC}.tar.gz)
 	(cd ${LIB_INSTALL_SRC} && tar -xzf ${APR_UTIL_SRC}.tar.gz)
 	(cd ${LIB_INSTALL_SRC}/${APR_UTIL_SRC} && AR="zig ar" RANLIB="zig ranlib" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --host=x86_64-linux --enable-shared=no --prefix=${APR_PREFIX} --with-apr=${APR_PREFIX} --with-expat=${EXPAT_PREFIX} && make -j $(nproc) && make install)
+fi
+
+if [[ ! -d "${PCRE_PREFIX}" ]]; then
+	(cd ${LIB_INSTALL_SRC} && [[ -f "${PCRE_SRC}.tar.gz" ]] || wget https://github.com/PCRE2Project/pcre2/releases/download/${PCRE_SRC}/${PCRE_SRC}.tar.gz)
+	(cd ${LIB_INSTALL_SRC} && tar -xzf ${PCRE_SRC}.tar.gz)
+	(cd ${LIB_INSTALL_SRC}/${PCRE_SRC} && AR="zig ar" RANLIB="zig ranlib" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --host=x86_64-linux --prefix=${PCRE_PREFIX} --enable-shared=no && make -j $(nproc) && make install)
 fi
 
 cmake -DCMAKE_BUILD_TYPE=${BUILD_CONF} -B ${BUILD_DIR} ${TOOLCHAIN}
