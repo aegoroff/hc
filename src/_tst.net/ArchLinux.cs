154902ea50d2d2168656c9443ef02d9cb6b7cb51
Alexander Egorov <egoroff@gmail.com>
2025-03-25 09:55:18 +0300

    ci: running linux integration tests during ci/cd

--- src/_tst.net/ArchLinux.cs
+++ src/_tst.net/ArchLinux.cs
@@ -1,4 +1,5 @@
 #if !WINDOWS
+using System;
 using System.Runtime.Versioning;
 
 namespace _tst.net;
@@ -6,10 +7,12 @@ namespace _tst.net;
 [SupportedOSPlatform("linux")]
 public class ArchLinux : Architecture
 {
-    protected override string RelativePath => "build";
+    protected override string RelativePath => Environment.GetEnvironmentVariable("PROJECT_BASE_PATH") == null
+                                                      ? "build"
+                                                      : $"build-x86_64-linux-gnu-{Configuration}";
 
     public override string Arch => "x64";
-    
+
     public override string Executable => "hc";
 }
 #endif
