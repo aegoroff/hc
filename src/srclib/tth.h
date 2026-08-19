acc5b1ea107f1e8cfa15733843b25c1fc423db47
Alexander Egorov <egoroff@gmail.com>
2026-08-19 09:22:10 +0300

    refactor(srclib): compute TTH with sphlib Tiger
    Drop the duplicate rhash Tiger implementation and its s-box table.

--- src/srclib/tth.h
+++ src/srclib/tth.h
@@ -1,25 +1,29 @@
 #ifndef TTH_H
 #define TTH_H
 
-#include "tiger.h"
+#include <stddef.h>
+#include <stdint.h>
+#include "sph_tiger.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define TTH_HASH_LENGTH 24
+#define TTH_LEAF_SIZE 1024
+
 /* algorithm context */
-typedef struct tth_ctx
-{
-	tiger_ctx tiger;       /* context used to hash tree leaves */
-	uint64_t block_count;  /* number of processed blocks */
-	uint64_t stack[64 * 3];
+typedef struct tth_ctx {
+	sph_tiger_context tiger; /* scratch hasher for leaf / node digests */
+	unsigned char leaf[TTH_LEAF_SIZE]; /* pending leaf payload (no marker) */
+	size_t leaf_len; /* bytes currently in leaf[] */
+	uint64_t block_count; /* number of processed leaves */
+	uint64_t stack[64 * 3]; /* pending node digests (24 bytes each) */
 } tth_ctx;
 
-/* hash functions */
-
 void rhash_tth_init(tth_ctx *ctx);
-void rhash_tth_update(tth_ctx *ctx, const unsigned char* msg, size_t size);
-void rhash_tth_final(tth_ctx *ctx, unsigned char result[24]);
+void rhash_tth_update(tth_ctx *ctx, const unsigned char *msg, size_t size);
+void rhash_tth_final(tth_ctx *ctx, unsigned char result[TTH_HASH_LENGTH]);
 
 #ifdef __cplusplus
 } /* extern "C" */
